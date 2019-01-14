7fdfe31dd61139b86e573209898a7757965fd264
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-14 12:41:32 -0700

    First commit with index.php file.

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Demo Cohort 23</title>
+	</head>
+
+	<body>
+		<p>Demo</p>
+
+		<?php
+/**
+ * Created by PhpStorm.
+ * User: jaerentredway
+ * Date: 2019-01-14
+ * Time: 12:36
+ */
+		?>
+	</body>
+</html>
+
+
+
+
